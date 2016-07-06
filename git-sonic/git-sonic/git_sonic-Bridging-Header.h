d00255f5d02985c59f2b6142cda1d11501c497f5
Alexis Katsaprakakis <alexis.katsaprakakis@inqbarna.com>
2016-07-06 12:11:59 +0200

    import libpd and GitUpKit
    It adds libpd's objc headers path to HEADER_SEARCH_PATHS.

---
+++ git-sonic/git-sonic/git_sonic-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  git_sonic-Bridging-Header.h
+//  git-sonic
+//
+//  Created by Alexis Katsaprakakis on 06/07/16.
+//  Copyright © 2016 phonegroove. All rights reserved.
+//
+
+
+#import "PdAudioController.h"
+#import <GitUpKit/GitUpKit.h>
