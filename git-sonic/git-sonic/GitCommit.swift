cd261cc3960bbaf2d330e957c6172223b218e8e2
hectr <h@mrhector.me>
2016-07-17 19:11:39 +0200

    create classes for git objects that conform to VCS protocols

---
+++ git-sonic/git-sonic/GitCommit.swift
@@ -0,0 +1,70 @@
+// GitCommit.swift
+//
+// Copyright (c) 2016 phonegroove. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+
+class GitCommit: ChangeSet {
+    
+    var SHA1: String
+    
+    var fullMessage: String
+    
+    var authorName: String
+    var authorEmail: String
+    var authorDate: NSDate
+    
+    var committerName: String
+    var committerEmail: String
+    var committerDate: NSDate
+    
+    var parents: [ChangeSet]
+    var children: [ChangeSet]
+    
+    var tippedBranches: [Branch]
+    var tags: [Tag]
+    
+    var fileChanges: [FileChange]
+    
+    var deletedLines: Int
+    var insertedLines: Int
+    var modifiedLines: Int
+    
+    init(SHA1: String, fullMessage: String, authorName: String, authorEmail: String, committerName: String, committerEmail: String, committerDate: NSDate, authorDate: NSDate, parents: [ChangeSet], children: [ChangeSet], tippedBranches: [Branch], tags: [Tag], fileChanges: [FileChange], deletedLines: Int, insertedLines: Int, modifiedLines: Int) {
+        
+        self.SHA1 = SHA1
+        self.fullMessage = fullMessage
+        self.authorName = authorName
+        self.authorEmail = authorEmail
+        self.authorDate = authorDate
+        self.committerName = committerName
+        self.committerEmail = committerEmail
+        self.committerDate = committerDate
+        self.parents = parents
+        self.children = children
+        self.tippedBranches = tippedBranches
+        self.tags = tags
+        self.fileChanges = fileChanges
+        self.deletedLines = deletedLines
+        self.insertedLines = insertedLines
+        self.modifiedLines = modifiedLines
+    }
+}
